458e382684eccf9b7d0b71bba09dea22e4f8aa83
jsakamoto <junichi.jp@gmail.com>
2015-03-06 10:51:57 +0900

    1st commit.

---
+++ build-vsix.ps1
@@ -0,0 +1,19 @@
+﻿# move current folder to where contains this .ps1 script file.
+$scriptDir = Split-Path $MyInvocation.MyCommand.Path
+pushd $scriptDir
+[System.Reflection.Assembly]::LoadFile((Convert-Path "Ionic.Zip.dll")) > $null
+
+# Get version infomation from reading manifest file.
+$manifest = [xml](cat .\extension.vsixmanifest -Raw)
+$ver = $manifest.PackageManifest.Metadata.Identity.Version
+
+# Create .vsix a package with embedding version information.
+$zip = new-object Ionic.Zip.ZipFile
+$zip.AddFile((Convert-Path '.\`[Content_Types`].xml'), "") > $null
+$zip.AddFile((Convert-Path .\extension.vsixmanifest), "") > $null
+$zip.AddFile((Convert-Path .\icon.png), "") > $null
+$zip.AddFile((Convert-Path .\registry.pkgdef), "") > $null
+$zip.AddDirectory((Convert-Path .\csharp), "csharp") > $null
+$zip.Save((Join-Path $scriptDir "CSharpMethodCodeSnippets.$ver.vsix"))
+#DEBUG: $zip.Save((Join-Path $scriptDir "CSharpMethodCodeSnippets.zip"))
+$zip.Dispose()
